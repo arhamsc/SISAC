f13d88de23097b4da0e634c5b472ad260d29d63d
arhamsc <shaik.arham@gmail.com>
2022-01-05 16:35:09 +0530

    book material CRUD added

--- controllers/stationary/stationary.js
+++ controllers/stationary/stationary.js
@@ -1,7 +1,9 @@
 //stationary controllers.
 const Availability = require('../../models/stationary/availability');
+const BookMaterial = require('../../models/stationary/book_material');
 
-const {ExpressError} = require('../../middleWare/error_handlers');
+const { cloudinary } = require('../../cloudinary');
+const { ExpressError } = require('../../middleWare/error_handlers');
 
 //to get available bluebooks and records
 module.exports.getAvailability = async(req, res, next) => {
@@ -24,5 +26,61 @@ module.exports.updateAvailability = async(req, res, next) => {
         res.json({message: "Availability Updated"});
     } catch(error) {
         next(new ExpressError('Could not update'), 500);
+    }
+}
+
+//* ***** Available Books Route ***** *//
+//fetch all books
+module.exports.getBooks = async(req, res, next) => {
+    try {
+        const books = await BookMaterial.find();
+        res.json({...books});
+    } catch(error) {
+        next(new ExpressError('Could not fetch the books'), 400);
+    }
+}
+
+//make new book material
+module.exports.addBook = async(req, res, next) => {
+    try {
+        const { book } = req.body;
+        const newBook = new BookMaterial({...book});
+        newBook.imageUrl = req.file.path;
+        newBook.imageFileName = req.file.filename;
+        await newBook.save();
+        res.json({newBook});
+    } catch(error) {
+        next(new ExpressError('Could not make new book'), 400);
+    }
+}
+
+//editing present book
+module.exports.editBook  = async(req, res, next) => {
+    try {
+        const  {bookId}  = req.params;
+        const { book } = req.body;
+        const oldBook = await BookMaterial.findById(bookId);
+        const newBook = await BookMaterial.findByIdAndUpdate(bookId, {...book}, {new: true});
+        if(req.file) {
+            await cloudinary.uploader.destroy(oldBook.imageFileName);
+            newBook.imageUrl = req.file.path;
+            newBook.imageFileName = req.file.filename;
+        }
+        await newBook.save();
+        res.json({newBook, message: "Book Edited"});
+    } catch(error) {
+        next(new ExpressError('Could not edit book'), 400 );
+    }
+}
+
+//route for deleting the book
+module.exports.deleteBook = async(req, res, next) => {
+    try{
+        const { bookId } = req.params;
+        const book = await BookMaterial.findByIdAndDelete(bookId);
+        await cloudinary.uploader.destroy(book.imageFileName);
+        res.json({message: "Book Deleted"});
+    } catch (error) {
+        next(new ExpressError("Could not delete", 400));
     }
 }
--- routes/stationary/stationary.js
+++ routes/stationary/stationary.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const router= express.Router()
 
+const multer = require('multer');
+const {storage} = require('../../cloudinary');
+const upload = multer({storage});
+
 const stationaryController = require('../../controllers/stationary/stationary');
 
 router.route('/availability')
     .get(stationaryController.getAvailability)
 
 router.route('/availability/:itemId')
-.patch(stationaryController.updateAvailability)
+    .patch(stationaryController.updateAvailability)
+
+router.route('/bookmaterial')
+    .get(stationaryController.getBooks)
+    .post(upload.single('image'), stationaryController.addBook)
+
+router.route('/bookmaterial/:bookId')
+    .patch(upload.single('image'), stationaryController.editBook)
+    .delete(stationaryController.deleteBook)
 
 
 module.exports = router;
